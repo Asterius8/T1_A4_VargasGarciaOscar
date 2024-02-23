45fe2f52685ea241cbb9ad713632e7ec0672160a
Asterius8 <oscar.vg@tecjerez.edu.mx>
2024-02-23 16:24:02 -0600

    Primer commit

---
+++ src/Practica_Eventos.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+class VentaPrincipal extends JFrame {
+	
+	
+	
+}
+
+public class Practica_Eventos {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
